4701dec2a7f8948bcdd1afbdf8475c578b8a213d
kout520 <3429630359@qq.com>
2026-04-16 19:33:21 +0800

    我更新了主控控制功能

---
+++ A_SZQ/MDK-ARM/A_SZQ/font.d
@@ -0,0 +1,4 @@
+a_szq\font.o: ..\code\font.c
+a_szq\font.o: ..\code\font.h
+a_szq\font.o: C:\keil5\Core\ARM\ARMCC\Bin\..\include\stdint.h
+a_szq\font.o: C:\keil5\Core\ARM\ARMCC\Bin\..\include\string.h
---
+++ A_SZQ/MDK-ARM/A_SZQ/startup_stm32f103xe.d
@@ -0,0 +1,1 @@
+a_szq\startup_stm32f103xe.o: startup_stm32f103xe.s
